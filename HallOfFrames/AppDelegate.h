1211950ca0cb3051b7d85e6ede71dfcd0c229c2d
Andrew Nguyen <anguy080@ucr.edu>
2015-07-22 13:59:45 -0700

    Initial Commit

---
+++ HallOfFrames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFrames
+//
+//  Created by Andrew  Nguyen on 7/22/15.
+//  Copyright (c) 2015 Andrew Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
